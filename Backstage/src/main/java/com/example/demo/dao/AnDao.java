2f0ac6f005f6d4badf7a31f22b9db47b5112223b
evol <1311128079@qq.com>
2020-08-24 18:42:08 +0800

    saojun

--- Backstage/src/main/java/com/example/demo/dao/AnDao.java
+++ Backstage/src/main/java/com/example/demo/dao/AnDao.java
@@ -4,7 +4,7 @@ import com.example.demo.entity.An;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
-
+//7
 public interface AnDao {
     @Select("select * from an where annumber='admin' and anpassword='123456'")
     public List<An> query(String annumber, String anpassword);
