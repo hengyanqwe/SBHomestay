a348f67c50f79e0426e8ccac5cfc781806bad52d
AREA-heart <2840713712@qq.com>
2020-08-24 20:16:59 +0800

    1

--- Backstage/src/main/java/com/example/demo/controller/AnController.java
+++ Backstage/src/main/java/com/example/demo/controller/AnController.java
@@ -4,6 +4,7 @@ import com.example.demo.dao.AnDao;
 import com.example.demo.entity.An;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -14,6 +15,7 @@ public class AnController {
     @Resource
     AnDao ad;
 
+    @ResponseBody
     @RequestMapping("query")
     public List<An> query(String annumber, String anpassword){
         List<An> list = ad.query(annumber, anpassword);
