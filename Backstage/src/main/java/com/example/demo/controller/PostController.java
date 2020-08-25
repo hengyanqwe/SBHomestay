0b3ac894828ec77e3ff9e0f7dc75f4fd1cf5c9c9
evol <1311128079@qq.com>
2020-08-25 08:51:53 +0800

    saojun

--- Backstage/src/main/java/com/example/demo/controller/PostController.java
+++ Backstage/src/main/java/com/example/demo/controller/PostController.java
@@ -4,6 +4,7 @@ import com.example.demo.dao.PostDao;
 import com.example.demo.entity.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -15,8 +16,31 @@ public class PostController {
     PostDao pd;
 
     @RequestMapping("query")
+    @ResponseBody
     public List<Post> query(){
         List<Post> list = pd.query();
+        System.out.println(list);
         return list;
+    }
+
+    @RequestMapping("add")
+    @ResponseBody
+    public int addPost(Post post){
+        int i = pd.addPost(post);
+        return i;
+    }
+
+    @RequestMapping("del")
+    @ResponseBody
+    public int del(Integer postId){
+        int i = pd.del(postId);
+        return i;
+    }
+
+    @RequestMapping("update")
+    @ResponseBody
+    public int update(Post post){
+        int i = pd.update(post);
+        return i;
     }
 }
--- Backstage/src/main/java/com/example/demo/dao/PostDao.java
+++ Backstage/src/main/java/com/example/demo/dao/PostDao.java
@@ -1,11 +1,18 @@
 package com.example.demo.dao;
 
 import com.example.demo.entity.Post;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
+@Mapper
 public interface PostDao {
     @Select("select * from post")
-    public List<Post> query();
+    List<Post> query();
+    @Delete("delete from post where postId = #{postId}")
+    int del(@Param("postId") Integer postId);
+    @Insert("insert into post (postName) values(#{postName})")
+    int addPost(@Param("post") Post post);
+    @Update("update post set postName = #{post.postName} where postId = #{post.postId}")
+    int update(@Param("post") Post post);
 }
