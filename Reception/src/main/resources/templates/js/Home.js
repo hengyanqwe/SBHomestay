94b6c93d7063156c5428cd87d6d55ce67e70019b
sqc <2367543602@qq.com>
2020-08-31 10:53:00 +0800

    前台-0831-1051

--- Reception/src/main/resources/templates/js/Home.js
+++ Reception/src/main/resources/templates/js/Home.js
@@ -35,5 +35,27 @@ function modalClose() {
     o_mask.style.visibility = "hidden";
 }
 
+function login() {
+    alert(1)
+
+    $.ajax({
+        //几个参数需要注意一下
+        type: "POST",//方法类型
+        dataType: "json",//预期服务器返回的数据类型
+        url: "/Home/login" ,//url
+        data: $('#form1').serialize(),
+        success: function (result) {
+            console.log(result);//打印服务端返回的数据(调试用)
+            if (result.resultCode == 200) {
+                alert("SUCCESS");
+            }
+        },
+        error : function() {
+            alert("异常！");
+        }
+    });
+}
+
+
 
 
